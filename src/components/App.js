7f8ea6b2c50b8faa83f04cbd5c4ca85158d0abdd
unknown <lsj8249@naver.com>
2021-10-28 21:08:28 +0900

    4.4 login

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import Router from "./Router";
 import Router from "components/Router"; // 절대 경로로 바꾸어주었다.
 import { authService } from "fbase";
 
 function App() {
   console.log(authService.currentUser); // null이 출력된다.
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  // const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   // 초기값은 false이고, setInLoggedIn은 변수가 아니라 isLoggedIn을 변경할 때 사용하는 함수
   // currentUser에 따라서 로그인 상태를 바꿔준다.
+  // setInterval(() => console.log(authService.currentUser), 2000); // 2초마다 실행하여 currentUser가 변하는 것을 확인한다.
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false);
+
+  // useEffect는 특정한 시점에 실행되는 함수.
+  // 파이어베이스 로그인 정보를 받게 되었을 때(파이어베이스가 초기화되는 시점)의 시점을 useEffect 함수로 잡아내어 로그인 완료 이후 보여줄 화면을 렌더링한다. -> onAuthStateChanged 사용
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []); // 1회만 동작
+
   return (
     <>
-      <Router isLoggedIn={isLoggedIn} />
+      {init ? <Router isLoggedIn={isLoggedIn} /> : "initializing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
       {/* JSX에 자바스크립트 코드를 삽입할 때는 코드를 중괄호로 감사줘야 한다. 
       &copy; 는 JSX에서 copyright 기호를 출력한다.*/}
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState(""); // 에러가 발생할 지점에 setError 함수를 사용한다.
 
   const onChange = (event) => {
     console.log(event.target.name); // email 입력과 password 입력을 구분하기 위해 2가지 함수를 만드는 방법보다 event.target.name으로 input 엘리먼트를 구분하는 방법이 더 효율적이다.
@@ -37,8 +38,12 @@ const Auth = () => {
     } catch (error) {
       // 실패
       console.log(error);
+      setError(error.message); // 에러 메시지는 error.message에 들어있다.
     }
   };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  // useState 함수에 함수를 인자로 전달하면 인자로 전달한 함수의 1번째 인자(prev)에 이전의 상태 넘어온다.
 
   return (
     <div>
@@ -60,7 +65,11 @@ const Auth = () => {
           onChange={onChange}
         />
         <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
         {/* firebase에 로그인 요청하는 버튼 -> 서버 호출 */}
         <button>Continue with Google</button>
